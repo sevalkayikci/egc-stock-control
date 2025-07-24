f9f3ca3ca952914606dd14d0e3c9c29e31fc0ee9
Seval Kayikci <66740624+sevalkayikci@users.noreply.github.com>
2025-07-24 23:31:33 +0300

    css1

--- cafe-stock-frontend/src/components/AddProduct.jsx
+++ cafe-stock-frontend/src/components/AddProduct.jsx
@@ -3,10 +3,11 @@ import axios from 'axios';
 import '../styles/AddProduct.css';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
 
 const AddProduct = () => {
   const [name, setName] = useState('');
